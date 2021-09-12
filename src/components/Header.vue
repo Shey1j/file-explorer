9436a51f3256fc68053c65332358eeb2644b952b
Shey1j <seyi.juliana@gmail.com>
2021-09-12 11:23:06 +0100

    create header component

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="explorer-header">
+    <nav>
+      <h1>My File Explorer</h1>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+.explorer-header {
+  width: 100%;
+  height: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid #8080804a;
+}
+
+h1 {
+  font-family: 'Pacifico', cursive;
+  font-size: 2rem;
+}
+</style>
